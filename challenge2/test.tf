458b67834a516d099209828e2f0bb70d2fdc7c99
estergo <53812787+estergo@users.noreply.github.com>
2023-01-03 11:19:38 +0200

    Create test.tf

---
+++ challenge2/test.tf
@@ -0,0 +1,66 @@
+provider "aws" {
+    region = "us-east-1"
+    profile = "dev3"
+}
+
+locals {
+  resource_prefix = {
+    value = "ester-drifts"
+  }
+}
+
+resource "aws_s3_bucket" "data" {
+  # bucket is public
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-data-ester-dev"
+  acl           = "public-read"
+  force_destroy = true
+  tags = merge({
+    Name        = "${local.resource_prefix.value}-data-ester-dev"
+    Environment = local.resource_prefix.value
+  }, {
+    git_commit           = "7f61b61d-920c-4b74-a81c-d45540873ec7"
+    git_file             = "terraform/aws/s3.tf"
+    git_last_modified_at = "2020-06-16 14:46:24"
+    git_last_modified_by = "411765@gmail.com"
+    git_modifiers        = "estergo"
+    git_org              = "bridgecrewio"
+    git_repo             = "terragoat"
+    yor_trace            = "48666bb2-3c3c-45ff-b0df-63bcf2afd22b"
+  })
+}
+
+
+resource "aws_s3_bucket_versioning" "data" {
+  bucket = aws_s3_bucket.data.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+
+resource "aws_s3_bucket" "data_log_bucket" {
+  bucket = "data-log-bucket"
+}
+
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "data_log_bucket" {
+  bucket = aws_s3_bucket.data_log_bucket.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
+
+resource "aws_s3_bucket_logging" "data" {
+  bucket = aws_s3_bucket.data.id
+
+  target_bucket = aws_s3_bucket.data_log_bucket.id
+  target_prefix = "log/"
+}
